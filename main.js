71db8806f51c944e52f0d9cb4ce5544f034b2dbe
Miguel Calderon <miguel070307@gmail.com>
2026-01-18 13:33:18 -0800

    initial setup

---
+++ main.js
@@ -0,0 +1,7 @@
+// JavaScript Learning Environment
+console.log("JavaScript environment ready!");
+
+console.log("Hello World");
+
+const output = document.getElementById('output');
+output.innerHTML = '<p>Start writing JavaScript code!</p>';
